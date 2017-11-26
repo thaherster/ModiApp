58b4bd36449650ffde595c2e73f7812e32576cde
thaher <thaherster@gmail.com>
2017-11-26 12:01:28 +0530

    initial commit

---
+++ Desktop/chatbot/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAB1kHocOhQBAJVoMKdj0YVNxvd4Dv6rSZBL4ilKyt1v7OZB2cVhlbRw0EaTORUZAIVcj2D6WMQsP69a77CFsZChglkFZBlAu1bOZCWQq9wWEoZCOO7aEBMjPus0YDDULP0F0OGA9b42eZC6hoH2njt1nTiY95VDeezRIKybsUQNMgZDZD',
+    FB_VERIFY_TOKEN: 'averiftytockenforfoodiedapp___ican',
+    API_AI_CLIENT_ACCESS_TOKEN: 'bf4a4c1e3b7a4dd7a2431c7df2e41250',
+    FB_APP_SECRET: 'dfac34c9b19c79001156f1e89f98a1ed',
+    SERVER_URL: "https://foodiechatbot.herokuapp.com/",
+
+};
