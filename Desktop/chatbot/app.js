32f424cc0d3311084a316d5ccc416e66ab10eda6
thaher <thaherster@gmail.com>
2017-11-27 15:03:53 +0530

    db insert

--- Desktop/chatbot/app.js
+++ Desktop/chatbot/app.js
@@ -755,6 +755,47 @@ function greetUserText(userId) {
 				console.log("FB user: %s %s, %s",
 					user.first_name, user.last_name, user.gender);
 
+				//connection pool
+				//a connection pool is a group of db connection
+				//sitting around waitig to be handled and used
+                var pool = new pg.Pool(config.PG_CONFIG);
+                pool.connect(function(err, client, done) {
+                    if (err) {
+                        return console.error('Error acquiring client', err.stack);
+                    }
+                    var rows = [];
+                    console.log('fetching user');
+                    client.query(`SELECT id FROM users WHERE fb_id='${userId}' LIMIT 1`,
+                        function(err, result) {
+                            console.log('query result ' + result);
+                            if (err) {
+                                console.log('Query error: ' + err);
+                            } else {
+                                console.log('rows: ' + result.rows.length);
+                                if (result.rows.length === 0) {
+                                    let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic, ' +
+                                        'locale, timezone, gender) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+                                    console.log('sql: ' + sql);
+                                    client.query(sql,
+                                        [
+                                            userId,
+                                            user.first_name,
+                                            user.last_name,
+                                            user.profile_pic,
+                                            user.locale,
+                                            user.timezone,
+                                            user.gender
+                                        ]);
+                                }
+                            }
+                        });
+
+                });
+                pool.end();
+
+
+
+
 				sendTextMessage(userId, "Welcome " + user.first_name + '!');
 			} else {
 				console.log("Cannot get data for fb user with id",
